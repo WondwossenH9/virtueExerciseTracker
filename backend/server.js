49ce4d034d55ad973f7d2984da8fe85de6094c07
wondata <wondwossenhailu7@gmail.com>
2024-09-09 22:34:58 +0300

    set up development environment and express app

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+
+const express = require('express')
+
+//express app
+const app = express()
+
+
+//global middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes react to requests
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the app' })
+})
+
+
+
+
+//listen for requests
+app.listen(process.env.PORT, () => {
+    console.log("listening on port", process.env.PORT)
+})
