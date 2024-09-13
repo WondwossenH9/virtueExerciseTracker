58de3046189e74f86b9ab4725c4f6d0c6c108423
wondata <wondwossenhailu7@gmail.com>
2024-09-13 21:08:45 +0300

    create database and successfuly connect to the database along with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
-
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 //express app
@@ -22,9 +22,18 @@ app.use((req, res, next) => {
 app.use('/api/workouts', workoutRoutes) //attach all routes to the app
 
 
+//database connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('connected to database and listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
 
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on port', process.env.PORT)
-})
+
+
