fd2a5626da3a125ae174445ba33ca27b82fca03c
wondata <wondwossenhailu7@gmail.com>
2024-09-13 18:15:53 +0300

    fix syntax error

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,27 +3,27 @@ const express = require('express') //to have access to the app from this file
 const router = express.Router() //create an instance of the router
 
 router.get('/', (req, res) => {
-    res.json({mssg: 'Get all workouts'})
- }) //attach a handle
+    res.json({ mssg: 'Get all workouts' })
+}) //attach a handle
 
 //GET a single workout
-router.get('/:id', (res, req) => {
-    res.json({mssg: 'GET a single workout'})
+router.get('/:id', (req, res) => {
+    res.json({ mssg: 'GET a single workout' })
 })
 
 //POST a new workout 
 router.post('/', (req, res) => {
-    res.json({mssg: 'POST a new workout' })
+    res.json({ mssg: 'POST a new workout' })
 })
 
 //DELETE a workout
 router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a workout'})
+    res.json({ mssg: 'DELETE a workout' })
 })
 
 //UPDATE a workout
 router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a workout'})
+    res.json({ mssg: 'UPDATE a workout' })
 })
 
 
