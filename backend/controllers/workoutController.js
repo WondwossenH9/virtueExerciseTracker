8138e5a0b3b9050473ab81863461b117aaeeeab5
wondata <wondwossenhailu7@gmail.com>
2024-09-16 16:06:55 +0300

    create controller function for deleting a workout

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -12,7 +12,7 @@ const getWorkout = async (req, res) => {
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(404).json({error: 'Workout does not exist'})
     }
-    
+
     const workout = await Workout.findById(id)
 
     if (!workout) {
@@ -32,10 +32,26 @@ const createWorkout = async (req, res) => {
     } //add a document to the database
 }
 //delete a workout
+const deleteWorkout = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'Workout does not exist.'})
+    }
+
+    const workout = await Workout.findOneAndDelete({ _id: id })
+    
+    if (!workout) {
+        return res.status(404).json({error: "Workout does not exist"})
+    }
+    res.status(200).json(workout)
+}
 //update a workout
+
 
 module.exports = {
     createWorkout,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout
 }
