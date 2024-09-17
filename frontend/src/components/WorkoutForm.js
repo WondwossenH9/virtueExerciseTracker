97f4473b00e2a8731f3a512782add3be72c7535e
wondata <wondwossenhailu7@gmail.com>
2024-09-17 19:58:53 +0300

    create styling, date format, and icons using external APIs

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -7,6 +7,7 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState('')
     const [reps, setReps] = useState('')
     const [error, setError] = useState(null)
+    const [emptyFields, setEmptyFields] = useState([])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -24,12 +25,14 @@ const WorkoutForm = () => {
 
         if (!response.ok) {
             setError(json.error)
+            setEmptyFields(json.emptyFields)
         }
         if (response.ok) {
             setTitle('') //reseting the form
             setLoad('') //resetting the form
             setReps('') //resetting the form
             setError(null)
+            setEmptyFields([])
             console.log("New workout created.", json)
             dispatch({ type: 'CREATE_WORKOUT', payload: json })
         }
@@ -43,12 +46,14 @@ const WorkoutForm = () => {
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes('title') ? 'error' : ''}
             />
             <label>Load (in KG): </label>
             <input
                 type="number"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes('load') ? 'error' : ''}
             />
 
             <label>Reps:</label>
@@ -56,6 +61,7 @@ const WorkoutForm = () => {
                 type="number"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes('reps') ? 'error' : ''}
             />
 
             <button>Create Workout</button>
@@ -63,5 +69,6 @@ const WorkoutForm = () => {
         </form>
     )
 }
+
 
 export default WorkoutForm
