f051287af1b11d8192eca536b37d7fc8b42d6ec9
wondata <wondwossenhailu7@gmail.com>
2024-09-17 21:44:39 +0300

    create user routes, controllers and models

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
-
+const userRoutes = require('./routes/user')
 //express app
 const app = express()
 
@@ -15,12 +15,10 @@ app.use((req, res, next) => {
     next()
 })
 
-//routes to test the API react to requests
-// app.get('/', (req, res) => {
-//     res.json({ mssg: 'Welcome to the app' })
-// })
-app.use('/api/workouts', workoutRoutes) //attach all routes to the app
+//routes
 
+app.use('/api/workouts', workoutRoutes) //attach all routes to the app
+app.use('/api/user', userRoutes)
 
 //database connection
 mongoose.connect(process.env.MONGO_URI)
