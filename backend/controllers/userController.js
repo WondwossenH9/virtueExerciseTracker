b4059b174ea5db01a689de0b78644d3ec9180463
wondata <wondwossenhailu7@gmail.com>
2024-09-17 23:37:42 +0300

    create tokens for signing in using jsonwebtoken package

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
 
+
+const createToken = (_id) => {
+    return jwt.sign({ _id: _id }, process.env.SECRET, { expiresIn: '3d' })
+}
 //login user
 const loginUser = async (req, res) => {
     res.json({ mssg: 'login user' })
@@ -13,8 +18,9 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, password)
+        const token = createToken(user._id) //create token
 
-        res.status(200).json({ email, user })
+        res.status(200).json({ email, token })
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
