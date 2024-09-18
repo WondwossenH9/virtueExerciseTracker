7044c8c2f0268e66a85e38f7bd60d7e6cde0d97a
wondata <wondwossenhailu7@gmail.com>
2024-09-18 09:21:27 +0300

    create user login hook

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -2,10 +2,10 @@ import { useAuthContext } from "./useAuthContext"
 
 export const useLogout = () => {
     const { dispatch } = useAuthContext()
-    
+
     const logout = () => {
         localStorage.removeItem('user') //to remove user
-        dispatch({type: 'LOGOUT'}) //dispatch logout action
+        dispatch({ type: 'LOGOUT' }) //dispatch logout action
     }
-    return {logout}
+    return { logout }
 }
