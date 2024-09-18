4977b733f813b46458ae13b44099954338653e02
wondata <wondwossenhailu7@gmail.com>
2024-09-18 08:46:12 +0300

    create custom hook in the react app to sign user up to the api endpoint at the backend

---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import { useAuthContext } from './useAuthContext'
+
+
+export const useSignup = () => {
+    const [error, setError] = useState(null) //state for error
+    const [isloading, setIsLoading] = useState(null) //state for loading
+    const { dispatch } = useAuthContext() //grab dispatch function to use the AuthContext
+
+    const signup = async (email, password) => {
+        setIsLoading(true)
+        setError(null)
+        //make post request
+        const response = await fetch('/api/user/signup', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email, password })
+        })
+        const json = await response.json() //to get json data response
+
+        if (!response.ok) {
+            setIsLoading(false)
+            setError(json.error)
+        }
+        if (response.ok) {
+            //save user to local storage
+            localStorage.setItem('user', JSON.stringify(json))
+
+            //update auth context
+            dispatch({ type: 'LOGIN', payload: json })
+            setIsLoading(false)
+        }
+
+    }
+    return { signup, isLoading, error }
+}
