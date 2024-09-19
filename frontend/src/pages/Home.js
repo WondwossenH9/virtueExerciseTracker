a7388df636ab38631505b4e98b923defb9ad0a79
wondata <wondwossenhailu7@gmail.com>
2024-09-19 17:36:44 +0300

    integrate goal tracking functionality into the Home component

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -6,6 +6,9 @@ import { useAuthContext } from '../hooks/useAuthContext'
 //components
 import WorkoutDetails from '../components/WorkoutDetails'
 import WorkoutForm from '../components/WorkoutForm'
+import GoalForm from '../components/GoalForm' //import goal form
+import GoalList from '../components/GoalList'
+import { GoalProvider } from '../context/GoalContext'
 
 const Home = () => {
     const { workouts, dispatch } = useWorkoutsContext()
@@ -36,6 +39,13 @@ const Home = () => {
                 ))}
             </div>
             <WorkoutForm />
+            //Wrap goal components in GoalProvider
+            <GoalProvider>
+                <h2>Your Goals</h2>
+                <GoalForm />
+                <GoalList />
+            </GoalProvider>
+
         </div>
     )
 }
