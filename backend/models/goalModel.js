ad9ca69f0fc924f588afe0752bc1354749115792
wondata <wondwossenhailu7@gmail.com>
2024-09-19 16:29:14 +0300

    create a new model for goals and define schema

---
+++ backend/models/goalModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');  
+
+const goalSchema = new mongoose.Schema({  
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },  
+    goalType: { type: String, required: true },  
+    targetValue: { type: Number, required: true },  
+    currentValue: { type: Number, default: 0 },  
+    startDate: { type: Date, default: Date.now },  
+    endDate: { type: Date, required: true },  
+});  
+
+module.exports = mongoose.model('Goal', goalSchema);
